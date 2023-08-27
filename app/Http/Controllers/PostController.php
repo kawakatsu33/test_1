f66d4ba0c121f7534da94f8e27e1adf14f5a4bef
kawakatsu33 <jibanyan3@icloud.com>
2023-08-27 17:51:17 +0900

    投稿作成画面の作成１

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,4 +16,10 @@ class PostController extends Controller
    {
        return view('posts.show', compact('post'));
    }
+
+    
+    public function create()
+    {
+        return view('posts/create');
+    }
 }
