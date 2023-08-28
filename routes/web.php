e927a9f5e852654e0b2b89e1c32ab9619498eda3
kawakatsu33 <jibanyan3@icloud.com>
2023-08-28 18:02:31 +0900

    新規作成で保存できるようなったよ

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,4 @@ use App\Http\Controllers\PostController;
 Route::get('/', [PostController::class,'index'])->name('posts.index');
 Route::get('/posts/create',[PostController::class,'create']);
 Route::get('/posts/{post}', [PostController::class, 'show']);
+Route::post('/posts',[PostController::class,'store']);
